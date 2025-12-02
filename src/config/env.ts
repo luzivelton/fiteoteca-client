59c8072edfe3287cc81439eea95c5ae76247c9fb
João Fernandes <joaolfern@proton.me>
2025-12-02 01:22:17 -0300

    feat: handle exception on empty env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,4 @@
 const FALLBACK_API_URL = 'https://fiteoteca-api.onrender.com'
 const VITE_DEV_API_URL = import.meta.env.VITE_DEV_API_URL
-if (!VITE_DEV_API_URL) {
-  throw new Error('VITE_DEV_API_URL is not defined')
-}
+
 export const API_URL = `${VITE_DEV_API_URL || FALLBACK_API_URL}/api`
